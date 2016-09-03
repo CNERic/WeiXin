17268e1e2c6fba684f5143f2c7bb4ef4cf156475
lzxz1234 <lzxz1234@gmail.com>
2016-09-03 18:49:49 +0800

    加自定义初始化

--- src/main/java/com/bj58/hrg/investment/wx/dto/App.java
+++ src/main/java/com/bj58/hrg/investment/wx/dto/App.java
@@ -1,5 +1,7 @@
 package com.bj58.hrg.investment.wx.dto;
 
+import org.apache.log4j.Logger;
+
 import com.bj58.hrg.investment.common.Cfg;
 
 /**
@@ -10,7 +12,22 @@ import com.bj58.hrg.investment.common.Cfg;
  */
 public class App {
 
-    private static Cfg cfg = Cfg.getClassPathCfg("/weixin.properties");
+    private static Logger log = Logger.getLogger(App.class);
+    private static Cfg cfg;
+    
+    static {
+        try {
+            cfg = Cfg.getClassPathCfg("/weixin.properties");
+        } catch (Exception e) {
+            log.warn("从 classpath 加载 weixin.properties 配置文件失败，请手动初始化 App.init()");
+        }
+    }
+    
+    public static void init(String path) {
+        
+        cfg = Cfg.getFileSystemCfg(path);
+        Info.reInit();
+    }
     
     public static String getConfig(String key) {
         
@@ -22,14 +39,14 @@ public class App {
         /**
          * id: appId
          */
-        public static final String id;
-        public static final String name;
-        public static final String secret;
-        public static final String aesKey;
-        public static final String token;
-        public static final String loginedUrl;
+        public static String id;
+        public static String name;
+        public static String secret;
+        public static String aesKey;
+        public static String token;
+        public static String loginedUrl;
         
-        static {
+        static void reInit() {
             id = cfg.get("weixin.app.id");
             name = cfg.get("weixin.app.name");
             secret = cfg.get("weixin.app.secret");
